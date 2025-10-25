59cb4d55f694327f1d8181934f4e73be54239020
Bùi Dương Tín <tinbui.koks@gmail.com>
2025-10-25 21:34:10 +0700

    Update next.config.js for basePath and assetPrefix
    Added basePath and assetPrefix for deployment.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: '/BuiDuongTin-2280603198-Blog',  // <-- THÊM DÒNG NÀY
+  assetPrefix: '/BuiDuongTin-2280603198-Blog', // <-- VÀ DÒNG NÀY
 }
 
-module.exports = nextConfig
+export default nextConfig
